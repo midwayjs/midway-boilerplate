9d9fc40375fdbe9be5d6400083c591c87bc185e1
waiting <1661926154@qq.com>
2019-11-20 16:56:01 +0800

    chore(ts-strict): update export style of plugin.ts

--- midway-ts-strict-boilerplate/boilerplate/src/config/plugin.ts
+++ midway-ts-strict-boilerplate/boilerplate/src/config/plugin.ts
@@ -1,4 +1,10 @@
-import { EggPlugin } from 'midway'
-export default {
-  static: true, // default is true
-} as EggPlugin
+
+export const cors = {
+  enable: false,
+  package: 'egg-cors',
+}
+
+
+// false 禁用全部安全检查用于临时调试
+// export const security = false
+
