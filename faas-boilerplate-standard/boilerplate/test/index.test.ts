8de5273281dc684cbf785bbf4e77f8ab87908ea3
Harry Chen <czy88840616@gmail.com>
2020-01-20 13:16:22 +0800

    chore: fix boilerplate

--- faas-boilerplate-standard/boilerplate/test/index.test.ts
+++ faas-boilerplate-standard/boilerplate/test/index.test.ts
@@ -6,7 +6,7 @@ describe('/test/index.test.ts', () => {
   it('invoke', async () => {
     const result: any = await invoke({
       functionName: 'index',
-      functionDir: join(__dirname, '../');
+      functionDir: join(__dirname, '../')
     });
     assert(/hello world/.test(result.body));
   });
