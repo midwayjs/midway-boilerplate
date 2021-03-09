6c05080214fdfe4a4470a7c4d7a84a822e545323
Gao Yang <echosoar@qq.com>
2021-03-09 16:58:20 +0800

    feat: v2 (#20)

---
+++ midway-boilerplate-list/index.js
@@ -0,0 +1,38 @@
+module.exports = {
+  web: {
+    desc: 'A web application boilerplate with midway',
+    package: '@midwayjs-examples/applicaiton-web',
+    group: 'Web Application',
+  },
+  koa: {
+    desc: 'A web application boilerplate with midway and koa',
+    package: '@midwayjs-examples/applicaiton-koa',
+    group: 'Web Application',
+  },
+  express: {
+    desc: 'A web application boilerplate with midway and express',
+    package: '@midwayjs-examples/applicaiton-express',
+    group: 'Web Application',
+  },
+  grpc: {
+    desc: 'Midway grpc boilerplate',
+    package: '@midwayjs-examples/application-grpc',
+    group: 'Web Application',
+  },
+  'koa-hooks-react': {
+    desc:
+      'The next generation of integrated front and back-end application development solutions',
+    package: '@midwayjs-examples/applicaiton-hooks-koa-react',
+    group: 'Web Application',
+  },
+  faas: {
+    desc: 'A serverless boilerplate for aliyun fc, tencent scf and so on',
+    package: '@midwayjs-examples/serverless-boilerplate-standard',
+    group: 'Serverless',
+  },
+  'faas-hooks-react': {
+    desc: 'A serverless boilerplate with react and use hooks',
+    package: '@midwayjs-examples/serverless-boilerplate-hooks-react',
+    group: 'Serverless',
+  },
+};
