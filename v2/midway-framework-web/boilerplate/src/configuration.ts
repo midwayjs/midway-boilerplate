ee52d2678393d3674878b0f26162cc379c3f4851
Harry Chen <czy88840616@gmail.com>
2021-04-07 09:26:50 +0800

    chore: update lint file

--- v2/midway-framework-web/boilerplate/src/configuration.ts
+++ v2/midway-framework-web/boilerplate/src/configuration.ts
@@ -4,15 +4,11 @@ import { Application } from 'egg';
 import { join } from 'path';
 
 @Configuration({
-  importConfigs: [
-    join(__dirname, './config'),
-  ],
+  importConfigs: [join(__dirname, './config')],
 })
 export class ContainerLifeCycle implements ILifeCycle {
-
   @App()
   app: Application;
 
-  async onReady() {
-  }
+  async onReady() {}
 }
