308d74432dd1df3b7c6503f47ea524dfc9846451
Harry Chen <czy88840616@gmail.com>
2021-04-08 19:02:40 +0800

    chore: update

---
+++ scripts/prepare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+cli=`pwd`/node_modules/.bin/mw
+tmpdir=`pwd`/tmp
+
+rm -rf $tmpdir
+mkdir -p $tmpdir
+
+pkgs=`find v2 -maxdepth 1 -mindepth 1`
+cwd=`pwd`
+for pkg in $pkgs
+do
+  cd $tmpdir
+  echo $cwd/v2/$pkg
+  $cli new ${pkg#*/} --template=$cwd/$pkg
+  cd ${pkg#*/}
+  npm run lint || exit 1
+  npm run test || exit 1
+done
