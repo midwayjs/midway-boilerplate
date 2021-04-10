822c1c771ea54f42e8e43dbfef50bd6301778012
Harry Chen <czy88840616@gmail.com>
2021-04-11 00:13:41 +0800

    chore: update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cli=`pwd`/node_modules/.bin/mw
+generator_script=`pwd`/scripts/generator.js
 tmpdir=`pwd`/tmp
 
 rm -rf $tmpdir
@@ -13,8 +13,9 @@ for pkg in $pkgs
 do
   cd $tmpdir
   echo $cwd/$pkg
-  $cli new ${pkg#*/} --template=$cwd/$pkg
+  node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
   cd ${pkg#*/}
+  npm install
   npm run lint || exit 1
   npm run test || exit 1
 done
