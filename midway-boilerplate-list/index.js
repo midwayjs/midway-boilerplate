c7d251d9fa382a91fd81d95cca6e972381708005
Harry Chen <czy88840616@gmail.com>
2021-04-12 19:36:58 +0800

    chore: add

--- midway-boilerplate-list/index.js
+++ midway-boilerplate-list/index.js
@@ -1,53 +1,61 @@
 module.exports = {
   web: {
-    desc: 'A web application boilerplate with midway and Egg.js',
-    package: '@midwayjs-examples/applicaiton-web',
-    group: 'Traditional Application',
+    desc: "A web application boilerplate with midway and Egg.js",
+    package: "@midwayjs-examples/applicaiton-web",
+    group: "Traditional Application",
   },
   koa: {
-    desc: 'A web application boilerplate with midway and koa',
-    package: '@midwayjs-examples/applicaiton-koa',
-    group: 'Traditional Application',
+    desc: "A web application boilerplate with midway and koa",
+    package: "@midwayjs-examples/applicaiton-koa",
+    group: "Traditional Application",
   },
   express: {
-    desc: 'A web application boilerplate with midway and express',
-    package: '@midwayjs-examples/applicaiton-express',
-    group: 'Traditional Application',
+    desc: "A web application boilerplate with midway and express",
+    package: "@midwayjs-examples/applicaiton-express",
+    group: "Traditional Application",
   },
   grpc: {
-    desc: 'Midway grpc boilerplate',
-    package: '@midwayjs-examples/application-grpc',
-    group: 'Traditional Application',
+    desc: "Midway grpc boilerplate",
+    package: "@midwayjs-examples/application-grpc",
+    group: "Traditional Application",
   },
   socketio: {
-    desc: 'Midway socket.io boilerplate',
-    package: '@midwayjs-examples/application-socketio',
-    group: 'Traditional Application',
+    desc: "Midway socket.io boilerplate",
+    package: "@midwayjs-examples/application-socketio",
+    group: "Traditional Application",
+  },
+  "web-socketio": {
+    desc: "Midway web application (Egg.js) with socket.io",
+    package: "@midwayjs-examples/application-web-socketio",
+    group: "Traditional Application",
   },
   faas: {
-    desc: 'A simple serverless boilerplate include http trigger for aliyun fc, tencent scf and so on',
-    package: '@midwayjs-examples/serverless-boilerplate-http',
-    group: 'Serverless',
-  },
-  'faas-all': {
-    desc: 'A serverless boilerplate include all trigger case for aliyun fc, tencent scf and so on',
-    package: '@midwayjs-examples/serverless-boilerplate-standard',
-    group: 'Serverless',
-  },
-  'faas-aggr': {
-    desc: 'A serverless http aggregation boilerplate for aliyun fc, tencent scf and so on',
-    package: '@midwayjs-examples/serverless-boilerplate-aggregation',
-    group: 'Serverless',
-  },
-  'koa-hooks-react': {
     desc:
-      'The next generation of integrated front and back-end application development solutions',
-    package: '@midwayjs-examples/applicaiton-hooks-koa-react',
-    group: 'Integration Application',
+      "A simple serverless boilerplate include http trigger for aliyun fc, tencent scf and so on",
+    package: "@midwayjs-examples/serverless-boilerplate-http",
+    group: "Serverless",
   },
-  'faas-hooks-react': {
-    desc: 'A serverless boilerplate with react and use hooks',
-    package: '@midwayjs-examples/serverless-boilerplate-hooks-react',
-    group: 'Integration',
+  "faas-all": {
+    desc:
+      "A serverless boilerplate include all trigger case for aliyun fc, tencent scf and so on",
+    package: "@midwayjs-examples/serverless-boilerplate-standard",
+    group: "Serverless",
+  },
+  "faas-aggr": {
+    desc:
+      "A serverless http aggregation boilerplate for aliyun fc, tencent scf and so on",
+    package: "@midwayjs-examples/serverless-boilerplate-aggregation",
+    group: "Serverless",
+  },
+  "koa-hooks-react": {
+    desc:
+      "The next generation of integrated front and back-end application development solutions",
+    package: "@midwayjs-examples/applicaiton-hooks-koa-react",
+    group: "Integration Application",
+  },
+  "faas-hooks-react": {
+    desc: "A serverless boilerplate with react and use hooks",
+    package: "@midwayjs-examples/serverless-boilerplate-hooks-react",
+    group: "Integration",
   },
 };
