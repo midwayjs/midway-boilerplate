6e5ab0d9f8cc613a3daf9ab561394cb748ddc574
Harry Chen <czy88840616@gmail.com>
2021-04-23 00:07:09 +0800

    chore: update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 set -e
 
+echo $1
 generator_script=`pwd`/scripts/generator.js
 tmpdir=`pwd`/tmp
 
 rm -rf $tmpdir
 mkdir -p $tmpdir
 
-pkgs=`find v2 -maxdepth 1 -mindepth 1`
-cwd=`pwd`
-for pkg in $pkgs
-do
+if [ -z "$1" ]
+then
+  pkgs=`find v2 -maxdepth 1 -mindepth 1`
+  cwd=`pwd`
+  for pkg in $pkgs
+  do
+    cd $tmpdir
+    echo $cwd/$pkg
+    node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
+    cd ${pkg#*/}
+    npm install
+    npm run lint || exit 1
+    npm run test || exit 1
+    echo $tmpdir/${pkg#*/}
+  done
+else
+  cwd=`pwd`
   cd $tmpdir
+  pkg=$1
   echo $cwd/$pkg
   node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
   cd ${pkg#*/}
   npm install
   npm run lint || exit 1
   npm run test || exit 1
-done
+fi
 
 echo 'complete'
