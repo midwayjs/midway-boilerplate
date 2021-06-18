b2f72cbebf2d5a0f80e76b37c45f215c50f9fbc2
Lxxyx <Lxxyxzj@gmail.com>
2021-06-18 14:19:39 +0800

    feat: update hooks examples

--- v2/midway-hooks-koa-react/boilerplate/src/apis/configuration.ts
+++ v2/midway-hooks-koa-react/boilerplate/src/apis/configuration.ts
@@ -1,9 +1,10 @@
-import { hooks, createConfiguration } from '@midwayjs/hooks'
-import { Application } from '@midwayjs/koa'
-import bodyParser from 'koa-bodyparser'
+import { hooks, createConfiguration } from '@midwayjs/hooks';
+import bodyParser from 'koa-bodyparser';
 
 export default createConfiguration({
-  imports: [hooks()],
-}).onReady((_, app: Application) => {
-  app.use(bodyParser())
-})
+  imports: [
+    hooks({
+      middleware: [bodyParser()],
+    }),
+  ],
+});
