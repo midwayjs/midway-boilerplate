ec943add4d05d98d98b4f608f332b396401fa8d3
Harry Chen <czy88840616@gmail.com>
2021-12-08 20:06:56 +0800

    feat: add egg v3 beta

---
+++ v3/midway-framework-web/README.md
@@ -0,0 +1,3 @@
+# midway boilerplate for web(eggjs)
+
+midway typescript 脚手架，用于标准 midway 应用。
---
+++ v3/midway-framework-web/boilerplate/src/config/config.default.ts
@@ -0,0 +1,17 @@
+import { EggAppConfig, PowerPartial } from 'egg';
+import { MidwayConfig, MidwayAppInfo } from '@midwayjs/core';
+
+export type DefaultConfig = PowerPartial<EggAppConfig>;
+
+export default (appInfo: MidwayAppInfo) => {
+  const config = {} as MidwayConfig & DefaultConfig;
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_{{keys}}';
+
+  // config.security = {
+  //   csrf: false,
+  // };
+
+  return config;
+};
