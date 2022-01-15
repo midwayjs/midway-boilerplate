0e260398ae471b6cab303d1dfeb3156da22ccb7f
Harry Chen <czy88840616@gmail.com>
2022-01-15 22:20:25 +0800

    feat: add express boilerplate

---
+++ v3/midway-framework-express/boilerplate/src/config/config.default.ts
@@ -0,0 +1,12 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+const config: MidwayConfig = {};
+
+// use for cookie sign key, should change to your own and keep security
+config.keys = '{{keys}}';
+
+config.express = {
+  port: 7001,
+};
+
+export default config;
