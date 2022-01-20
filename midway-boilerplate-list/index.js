888f5f1e1e3d1bbbab8f57b3c06da6fc9e48f024
Harry Chen <czy88840616@gmail.com>
2022-01-20 23:57:47 +0800

    chore: upgrade version

--- midway-boilerplate-list/index.js
+++ midway-boilerplate-list/index.js
@@ -4,61 +4,38 @@ module.exports = {
     package: "@midwayjs-examples/application-koa-v3",
     group: "v3",
   },
-  "egg-v3": {
-    desc: "A web application boilerplate with midway v3(egg)",
-    package: "@midwayjs-examples/application-web-v3",
-    group: "v3",
-  },
-  "express-v3": {
-    desc: "A web application boilerplate with midway v3(express)",
-    package: "@midwayjs-examples/application-express-v3",
-    group: "v3",
-  },
+  // "egg-v3": {
+  //   desc: "A web application boilerplate with midway v3(egg)",
+  //   package: "@midwayjs-examples/application-web-v3",
+  //   group: "v3",
+  // },
+  // "express-v3": {
+  //   desc: "A web application boilerplate with midway v3(express)",
+  //   package: "@midwayjs-examples/application-express-v3",
+  //   group: "v3",
+  // },
   web: {
     desc: "A web application boilerplate with midway and Egg.js",
     package: "@midwayjs-examples/application-web",
-    group: "Traditional Application",
+    group: "v2",
   },
   koa: {
     desc: "A web application boilerplate with midway and koa",
     package: "@midwayjs-examples/application-koa",
-    group: "Traditional Application",
+    group: "v2",
   },
   express: {
     desc: "A web application boilerplate with midway and express",
     package: "@midwayjs-examples/application-express",
-    group: "Traditional Application",
+    group: "v2",
   },
   faas: {
     desc:
       "A simple serverless boilerplate include http trigger for aliyun fc, tencent scf and so on",
     package: "@midwayjs-examples/serverless-boilerplate-http",
-    group: "Serverless",
+    group: "v2",
   },
-  "faas-all": {
-    desc:
-      "A serverless boilerplate include all trigger case for aliyun fc, tencent scf and so on",
-    package: "@midwayjs-examples/serverless-boilerplate-standard",
-    group: "Serverless",
-  },
-  "faas-aggr": {
-    desc:
-      "A serverless http aggregation boilerplate for aliyun fc, tencent scf and so on",
-    package: "@midwayjs-examples/serverless-boilerplate-aggregation",
-    group: "Serverless",
-  },
-  "koa-hooks-react": {
-    desc:
-      "The next generation of integrated front and back-end application development solutions",
-    package: "@midwayjs-examples/applicaiton-hooks-koa-react",
-    group: "Integration",
-  },
-  "faas-hooks-react": {
-    desc: "A serverless boilerplate with react and use hooks",
-    package: "@midwayjs-examples/serverless-boilerplate-hooks-react",
-    group: "Integration",
-  },
-  "component": {
+  component: {
     desc: "A midway component boilerplate",
     package: "@midwayjs-examples/component",
     group: "Extension",
