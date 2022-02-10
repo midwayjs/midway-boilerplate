87a7e67165f4c03b70006cbd6461f0bf09d2f479
Harry Chen <czy88840616@gmail.com>
2022-02-10 15:59:31 +0800

    chore: add component boilerplate (#47)

--- midway-boilerplate-list/index.js
+++ midway-boilerplate-list/index.js
@@ -4,21 +4,21 @@ module.exports = {
     package: "@midwayjs-examples/application-koa-v3",
     group: "v3",
   },
+  "egg-v3": {
+    desc: "A web application boilerplate with midway v3(egg)",
+    package: "@midwayjs-examples/application-web-v3",
+    group: "v3",
+  },
   "faas-v3": {
     desc: "A serverless application boilerplate with midway v3(faas)",
     package: "@midwayjs-examples/serverless-boilerplate-http-v3",
     group: "v3",
   },
-  // "egg-v3": {
-  //   desc: "A web application boilerplate with midway v3(egg)",
-  //   package: "@midwayjs-examples/application-web-v3",
-  //   group: "v3",
-  // },
-  // "express-v3": {
-  //   desc: "A web application boilerplate with midway v3(express)",
-  //   package: "@midwayjs-examples/application-express-v3",
-  //   group: "v3",
-  // },
+  "component-v3": {
+    desc: "A midway component boilerplate for v3",
+    package: "@midwayjs-examples/component-v3",
+    group: "Extension",
+  },
   web: {
     desc: "A web application boilerplate with midway and Egg.js",
     package: "@midwayjs-examples/application-web",
@@ -29,11 +29,6 @@ module.exports = {
     package: "@midwayjs-examples/application-koa",
     group: "v2",
   },
-  express: {
-    desc: "A web application boilerplate with midway and express",
-    package: "@midwayjs-examples/application-express",
-    group: "v2",
-  },
   faas: {
     desc:
       "A simple serverless boilerplate include http trigger for aliyun fc, tencent scf and so on",
@@ -41,7 +36,7 @@ module.exports = {
     group: "v2",
   },
   component: {
-    desc: "A midway component boilerplate",
+    desc: "A midway component boilerplate for v2",
     package: "@midwayjs-examples/component",
     group: "Extension",
   },
