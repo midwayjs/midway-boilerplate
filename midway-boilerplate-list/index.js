dd7d0723d97189761243fc0a3ac0edd9cbe09ea8
Harry Chen <czy88840616@gmail.com>
2022-02-21 21:55:24 +0800

    chore: update

--- midway-boilerplate-list/index.js
+++ midway-boilerplate-list/index.js
@@ -9,6 +9,12 @@ module.exports = {
     package: "@midwayjs-examples/application-web-v3",
     group: "v3",
   },
+  "express-v3": {
+    desc: "A web application boilerplate with midway v3(express)",
+    package: "@midwayjs-examples/application-express-v3",
+    group: "v3",
+    hidden: true,
+  },
   "faas-v3": {
     desc: "A serverless application boilerplate with midway v3(faas)",
     package: "@midwayjs-examples/serverless-boilerplate-http-v3",
@@ -17,7 +23,7 @@ module.exports = {
   "component-v3": {
     desc: "A midway component boilerplate for v3",
     package: "@midwayjs-examples/component-v3",
-    group: "Extension",
+    group: "v3",
   },
   web: {
     desc: "A web application boilerplate with midway and Egg.js",
@@ -29,15 +35,61 @@ module.exports = {
     package: "@midwayjs-examples/application-koa",
     group: "v2",
   },
+  express: {
+    desc: "A web application boilerplate with midway and express",
+    package: "@midwayjs-examples/application-express",
+    group: "v2",
+    hidden: true,
+  },
+  grpc: {
+    desc: "Midway grpc boilerplate",
+    package: "@midwayjs-examples/application-grpc",
+    group: "v2",
+    hidden: true,
+  },
+  socketio: {
+    desc: "Midway socket.io boilerplate",
+    package: "@midwayjs-examples/application-socketio",
+    group: "v2",
+    hidden: true,
+  },
+  "web-socketio": {
+    desc: "Midway web application (Egg.js) with socket.io",
+    package: "@midwayjs-examples/application-web-socketio",
+    group: "v2",
+    hidden: true,
+  },
+  "web-grpc": {
+    desc: "Midway web application (Egg.js) with grpc server",
+    package: "@midwayjs-examples/application-web-grpc",
+    group: "v2",
+    hidden: true,
+  },
   faas: {
     desc:
       "A simple serverless boilerplate include http trigger for aliyun fc, tencent scf and so on",
     package: "@midwayjs-examples/serverless-boilerplate-http",
     group: "v2",
+    hidden: true,
+  },
+  "faas-all": {
+    desc:
+      "A serverless boilerplate include all trigger case for aliyun fc, tencent scf and so on",
+    package: "@midwayjs-examples/serverless-boilerplate-standard",
+    group: "v2",
+    hidden: true,
+  },
+  "faas-aggr": {
+    desc:
+      "A serverless http aggregation boilerplate for aliyun fc, tencent scf and so on",
+    package: "@midwayjs-examples/serverless-boilerplate-aggregation",
+    group: "v2",
+    hidden: true,
   },
   component: {
     desc: "A midway component boilerplate for v2",
     package: "@midwayjs-examples/component",
-    group: "Extension",
+    group: "v2",
+    hidden: true,
   },
 };
