dedea1e8eb26722b43380969787143c37544a395
Harry Chen <czy88840616@gmail.com>
2023-04-18 23:52:09 +0800

    chore: add mock for quickstart (#56)
    * chore: add mock for quickstart
    
    * chore: remove nock

--- v3/midway-quick-start/boilerplate/test/controller/weather.test.ts
+++ v3/midway-quick-start/boilerplate/test/controller/weather.test.ts
@@ -1,38 +1,16 @@
 import { createApp, close, createHttpRequest } from '@midwayjs/mock';
 import { Framework, Application } from '@midwayjs/koa';
-import * as nock from 'nock';
 
 describe('test/controller/weather.test.ts', () => {
   let app: Application;
   beforeAll(async () => {
     // create app
     app = await createApp<Framework>();
-    // 这里由于 github 测试环境无法顺畅的连通国内网络，使用 nock 这个库模拟了服务，实际测试不需要
-    nock('http://www.weather.com.cn')
-      .get('/data/sk/101010100.html')
-      .reply(200, {
-        weatherinfo: {
-          city: '北京',
-          cityid: '101010100',
-          temp: '27.9',
-          WD: '南风',
-          WS: '小于3级',
-          SD: '28%',
-          AP: '1002hPa',
-          njd: '暂无实况',
-          WSE: '<3',
-          time: '17:55',
-          sm: '2.1',
-          isRadar: '1',
-          Radar: 'JC_RADAR_AZ9010_JB',
-        },
-      });
   });
 
   afterAll(async () => {
     // close app
     await close(app);
-    nock.restore();
   });
 
   it('should test /weather with success request', async () => {
