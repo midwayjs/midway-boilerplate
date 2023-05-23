750af0457628141a43bd8b2508055084b5d21e73
Harry Chen <czy88840616@gmail.com>
2023-05-23 16:46:19 +0800

    chore: remove v2

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -11,21 +11,6 @@ mkdir -p $tmpdir
 
 if [ -z "$1" ]
 then
-  # v2
-  pkgs=`find v2 -maxdepth 1 -mindepth 1`
-  cwd=$origin_cwd
-  for pkg in $pkgs
-  do
-    cd $tmpdir
-    echo $cwd/$pkg
-    node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
-    cd ${pkg#*/}
-    npm install
-    npm run lint --if-present || exit 1
-    npm run test || exit 1
-    echo $tmpdir/${pkg#*/}
-  done
-
   # v3
   cwd=$origin_cwd
   cd $origin_cwd
