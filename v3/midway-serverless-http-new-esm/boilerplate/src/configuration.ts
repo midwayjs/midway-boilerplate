f1585f560e32043821b2560230ae640a70e16b58
Harry Chen <czy88840616@gmail.com>
2023-10-17 13:15:07 +0800

    fix: config typo

--- v3/midway-serverless-http-new-esm/boilerplate/src/configuration.ts
+++ v3/midway-serverless-http-new-esm/boilerplate/src/configuration.ts
@@ -1,13 +1,13 @@
 import { Configuration, ILifeCycle } from '@midwayjs/core';
 import * as faas from '@midwayjs/faas';
-import DefulatConfig from './config/config.default.js';
+import DefaultConfig from './config/config.default.js';
 import ProdConfig from './config/config.prod.js';
 
 @Configuration({
   imports: [faas],
   importConfigs: [
     {
-      default: DefulatConfig,
+      default: DefaultConfig,
       prod: ProdConfig,
     },
   ],
