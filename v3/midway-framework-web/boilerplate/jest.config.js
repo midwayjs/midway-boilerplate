9df6a99f2b2be08fb5b0154980a5b84f5286a128
Harry Chen <czy88840616@gmail.com>
2024-01-17 15:12:21 +0800

    chore: update

--- v3/midway-framework-web/boilerplate/jest.config.js
+++ v3/midway-framework-web/boilerplate/jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFiles: ['ts-node/register'],
 };
