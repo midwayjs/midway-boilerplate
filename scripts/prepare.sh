91dd5c5b7571ab9c74dbef7e149ea586e13cbc66
Harry Chen <czy88840616@gmail.com>
2025-07-06 23:23:42 +0800

    feat: add v4 template (#74)
    * feat: add v4 template
    
    * test: fix v3/v4
    
    * feat: Added Node.js CI workflow support for v3 and v4 builds
    
    * chore: update
    
    * fix: remove pnm
    
    * fix: dir
    
    * chore: update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -9,38 +9,40 @@ tmpdir=$origin_cwd/tmp
 rm -rf $tmpdir
 mkdir -p $tmpdir
 
-if [ -z "$1" ]
+run_for_pkg() {
+  pkg_path=$1
+  pkg_name=${pkg_path#*/}
+  cd $tmpdir
+  echo $origin_cwd/$pkg_path
+  node $generator_script $tmpdir/$pkg_name $origin_cwd/$pkg_path
+  cd $pkg_name
+  npm install
+  npm run lint --if-present || exit 1
+  npm run test || exit 1
+  echo $tmpdir/$pkg_name
+}
+
+if [ ! -z "$PKG_ROOT" ] && { [ "$PKG_ROOT" = "v3" ] || [ "$PKG_ROOT" = "v4" ]; }
 then
-  # v3
   cwd=$origin_cwd
   cd $origin_cwd
-
   rm -rf $tmpdir
   mkdir -p $tmpdir
-
-  pkgs=`find v3 -maxdepth 1 -mindepth 1`
-
+  pkgs=`find $PKG_ROOT -maxdepth 1 -mindepth 1`
   for pkg in $pkgs
   do
-    cd $tmpdir
-    echo $cwd/$pkg
-    node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
-    cd ${pkg#*/}
-    npm install
-    npm run lint --if-present || exit 1
-    npm run test || exit 1
-    echo $tmpdir/${pkg#*/}
+    run_for_pkg $pkg
   done
+elif [ -z "$1" ]
+then
+  # 兼容老逻辑，默认不处理
+  echo "请通过 PKG_ROOT 指定目录或传入具体包路径"
+  exit 1
 else
   cwd=$origin_cwd
   cd $tmpdir
   pkg=$1
-  echo $cwd/$pkg
-  node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
-  cd ${pkg#*/}
-  npm install
-  npm run lint --if-present || exit 1
-  npm run test || exit 1
+  run_for_pkg $pkg
 fi
 
 echo 'complete'
