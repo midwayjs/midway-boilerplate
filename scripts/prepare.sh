4fd29c08ab2142482c570087ac4f17ba6f5efc2e
Harry Chen <czy88840616@gmail.com>
2025-07-06 22:36:15 +0800

    test: fix v3/v4

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -9,38 +9,43 @@ tmpdir=$origin_cwd/tmp
 rm -rf $tmpdir
 mkdir -p $tmpdir
 
+run_for_pkg() {
+  pkg_path=$1
+  pkg_name=${pkg_path#*/}
+  cd $tmpdir
+  echo $origin_cwd/$pkg_path
+  node $generator_script $tmpdir/$pkg_name $origin_cwd/$pkg_path
+  cd $pkg_name
+  pnpm install
+  pnpm run lint --if-present || exit 1
+  pnpm run test || exit 1
+  echo $tmpdir/$pkg_name
+}
+
 if [ -z "$1" ]
 then
-  # v3
+  # v3 & v4
   cwd=$origin_cwd
   cd $origin_cwd
 
   rm -rf $tmpdir
   mkdir -p $tmpdir
 
-  pkgs=`find v3 -maxdepth 1 -mindepth 1`
-
-  for pkg in $pkgs
+  for ver in v3 v4
   do
-    cd $tmpdir
-    echo $cwd/$pkg
-    node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
-    cd ${pkg#*/}
-    npm install
-    npm run lint --if-present || exit 1
-    npm run test || exit 1
-    echo $tmpdir/${pkg#*/}
+    if [ -d "$ver" ]; then
+      pkgs=`find $ver -maxdepth 1 -mindepth 1`
+      for pkg in $pkgs
+      do
+        run_for_pkg $pkg
+      done
+    fi
   done
 else
   cwd=$origin_cwd
   cd $tmpdir
   pkg=$1
-  echo $cwd/$pkg
-  node $generator_script $tmpdir/${pkg#*/} $cwd/$pkg
-  cd ${pkg#*/}
-  npm install
-  npm run lint --if-present || exit 1
-  npm run test || exit 1
+  run_for_pkg $pkg
 fi
 
 echo 'complete'
